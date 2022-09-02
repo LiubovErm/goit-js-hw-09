76e8d6662834bb382f88cb1aa54ae7c38075f391
LiubovErm <105315327+LiubovErm@users.noreply.github.com>
2022-09-02 09:45:34 +0000

    Deploying to gh-pages from @ LiubovErm/goit-js-hw-09@1cb40312f5efc676d8a46f124bad92b559f35db0 🚀

---
+++ 01-color-switcher.34f5e29c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled","disabled"),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(timerId),e.setAttribute("disabled","disabled"),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.34f5e29c.js.map
