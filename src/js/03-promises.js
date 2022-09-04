1d30ffcd8edcd8d2b94c25a9217ce3985ecb047a
LiubovErm <105315327+LiubovErm@users.noreply.github.com>
2022-09-04 21:22:38 +0300

    create task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onClickSubmit);
+
+function onClickSubmit(event) {
+  event.preventDefault();
+
+
+  
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
